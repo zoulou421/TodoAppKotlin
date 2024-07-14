2870e218266c016fbef602b8b20d10444862e2ba
Bonevy BEBY <bonevybeby@gmail.com>
2024-07-14 19:51:46 +0200

    added: Textfield and Button

--- app/src/main/java/com/formationkilo/todoapp/TodoListPage.kt
+++ app/src/main/java/com/formationkilo/todoapp/TodoListPage.kt
@@ -4,18 +4,26 @@ import android.annotation.SuppressLint
 import android.os.Build
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -34,12 +42,29 @@ import java.util.Locale
 @RequiresApi(Build.VERSION_CODES.O)
 fun TodoListPage(){
  var todoList= getFakeTodo()
+    var inputText by remember {
+        mutableStateOf("")
+    }
    // Text(text = todoList.toString())
     Column(
         modifier = Modifier
             .fillMaxSize()
             .padding(8.dp)
     ) {
+        Row(modifier = Modifier.fillMaxWidth()
+            .padding(8.dp),
+            horizontalArrangement = Arrangement.SpaceEvenly) {
+            OutlinedTextField(
+                modifier = Modifier.weight(1f),
+                value =inputText ,
+                onValueChange = {
+                    inputText=it
+                }
+            )
+            Button(onClick = { /*TODO*/ }) {
+               Text(text = "Add")
+            }
+        }
         LazyColumn (
             content = {
                 itemsIndexed(todoList){index: Int, item: Todo ->  
