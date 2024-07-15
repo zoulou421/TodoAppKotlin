d0842b8c0444085b4a77b3229610aca2f17f78b6
Bonevy BEBY <bonevybeby@gmail.com>
2024-07-15 17:34:27 +0200

    error: Cannot figure out how to save this field into database. You can consider adding a type converter for it.     private java.util.Date creatededAt;

---
+++ app/src/main/java/com/formationkilo/todoapp/MainApplication.kt
@@ -0,0 +1,20 @@
+package com.formationkilo.todoapp
+
+import android.app.Application
+import androidx.room.Room
+import com.formationkilo.todoapp.db.TodoDatabase
+
+class MainApplication:Application() {
+    companion object{
+        lateinit var  todoDatabase: TodoDatabase
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+      todoDatabase=  Room.databaseBuilder(
+            applicationContext,
+            TodoDatabase::class.java,
+            TodoDatabase.NAME
+        ).build()
+    }
+}
--- app/src/main/java/com/formationkilo/todoapp/TodoViewModel.kt
+++ app/src/main/java/com/formationkilo/todoapp/TodoViewModel.kt
@@ -1,16 +1,20 @@
 package com.formationkilo.todoapp
 
+import android.icu.text.CaseMap.Title
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import java.time.Instant
+import java.util.Date
 
 
 class TodoViewModel: ViewModel(){
-  private var _todoList=MutableLiveData<List<Todo>>()
+ // private var _todoList=MutableLiveData<List<Todo>>()
+val todoDao=MainApplication.todoDatabase.getTodoDao()
+  //val todoList:LiveData<List<Todo>>=_todoList
+  val todoList:LiveData<List<Todo>> =todoDao.getAllTodo_v2()
 
-  val todoList:LiveData<List<Todo>> =_todoList
-
-
+/*
   fun getAllTodo(){
      _todoList.value=TodoManager.getAllTodo().reversed()
   }
@@ -22,5 +26,11 @@ class TodoViewModel: ViewModel(){
    TodoManager.deleteTodo(id)
     getAllTodo()
 
-  }
+  }*/
+    fun addTodo(title: String){
+        todoDao.addTodo_v2(Todo(title=title, creatededAt = Date.from(Instant.now())))
+    }
+    fun deleteTodo(id:Int){
+        todoDao.deleteTodo_v2(id)
+    }
 }
